378ef621d5c44e53b9164c7bba25c65d4b490778
NunoMCP <nunomcp1998@gmail.com>
2019-10-27 17:43:30 +0000

    add source pdf

--- extract.py
+++ extract.py
@@ -1,3 +1,5 @@
 import camelot
 tables = camelot.read_pdf('horario.pdf')
-tables.export("horario.html", f="html", compress=True)
+# tables.export("horario.html", f="html",)
+print(tables[0][1])
+print(tables[1])
